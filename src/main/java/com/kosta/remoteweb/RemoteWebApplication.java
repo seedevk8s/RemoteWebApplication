e5a86db0a0615c8087a6df7dd02b2958599fc733
seedevk8s <seedevk8s@gmail.com>
2024-08-02 17:01:50 +0900

    pom.xml 의존성 라이브러리 2개 설치 view단 prefix, suffix 설정정보 추가 jsp 뷰, java 컨트롤러 추가 http://localhost:8080/hello 테스트

---
+++ src/main/java/com/kosta/remoteweb/RemoteWebApplication.java
@@ -0,0 +1,13 @@
+package com.kosta.remoteweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemoteWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RemoteWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kosta/remoteweb/RemoteWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kosta.remoteweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemoteWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
